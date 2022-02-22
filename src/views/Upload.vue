129be98da4fc5e097760aa6b1e947a277d3c37ee
Camden <camdencparker@gmail.com>
2022-02-22 07:15:23 -0700

    still unable to get uppy api to work, problem when calling Core.method on Uppy

---
+++ src/views/Upload.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="upload">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+import Uppy, { XHRUpload, DragDrop } from 'uppy'
+
+// And their styles (for UI plugins)
+// With webpack and `style-loader`, you can import them like this:
+import '@uppy/core/dist/style.css'
+import '@uppy/dashboard/dist/style.css'
+
+const uppy = new Uppy()
+  .use(Dashboard, {
+    trigger: '#select-files',
+  })
+  .use(XHRUpload, { endpoint: 'https://api2.transloadit.com' })
+
+uppy.on('complete', (result) => {
+  console.log('Upload complete! We’ve uploaded these files:', result.successful)
+})
+</script>
